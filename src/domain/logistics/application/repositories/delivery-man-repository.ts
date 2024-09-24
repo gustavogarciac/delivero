c8ef6e584b6cddce5c3c364409ff00547d5fbc41
gustavo <gustavogarciaci1@gmail.com>
2024-09-23 21:39:06 -0300

    feat: error handling

--- src/domain/logistics/application/repositories/delivery-man-repository.ts
+++ src/domain/logistics/application/repositories/delivery-man-repository.ts
@@ -2,5 +2,7 @@ import { DeliveryMan } from "../../enterprise/entities/delivery-man";
 
 export abstract class DeliveryMenRepository {
   abstract findByEmail(email: string): Promise<DeliveryMan | null>
+  abstract findById(id: string): Promise<DeliveryMan | null>
   abstract create(deliveryMan: DeliveryMan): Promise<void>
+  abstract delete(deliveryMan: DeliveryMan): Promise<void>
 }
--- src/domain/logistics/application/use-cases/create-delivery-man.ts
+++ src/domain/logistics/application/use-cases/create-delivery-man.ts
@@ -1,6 +1,7 @@
 import { Either, left, right } from "@/core/either"
 import { DeliveryMenRepository } from "../repositories/delivery-man-repository"
 import { DeliveryMan } from "../../enterprise/entities/delivery-man"
+import { BadRequestError } from "@/core/errors/bad-request-error"
 
 interface CreateDeliveryManUseCaseRequest {
   name: string
@@ -8,7 +9,7 @@ interface CreateDeliveryManUseCaseRequest {
   password: string
 }
 
-type CreateDeliveryManUseCaseResponse = Either<Error, { deliveryMan: DeliveryMan }>
+type CreateDeliveryManUseCaseResponse = Either<BadRequestError, { deliveryMan: DeliveryMan }>
 
 export class CreateDeliveryManUseCase {
   constructor(private deliveryMenRepository: DeliveryMenRepository) {}
@@ -21,7 +22,7 @@ export class CreateDeliveryManUseCase {
     const deliveryManWithExistingEmail = await this.deliveryMenRepository.findByEmail(email)
 
     if(deliveryManWithExistingEmail) {
-      return left(new Error)
+      return left(new BadRequestError)
     }
 
     const deliveryMan = DeliveryMan.create({
