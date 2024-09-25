7970b974aac1426a32006af9660acc4ce211c936
gustavo <gustavogarciaci1@gmail.com>
2024-09-24 21:37:39 -0300

    feat(): get delivery man profile

---
+++ src/domain/logistics/application/use-cases/get-delivery-man-profile.ts
@@ -0,0 +1,26 @@
+import { Either, left, right } from "@/core/either"
+import { DeliveryMenRepository } from "../repositories/delivery-man-repository"
+import { BadRequestError } from "@/core/errors/bad-request-error"
+import { ResourceNotFoundError } from "@/core/errors/resource-not-found-error"
+
+interface GetDeliveryManProfileUseCaseRequest {
+  deliveryManId: string
+}
+
+type GetDeliveryManProfileUseCaseResponse = Either<BadRequestError, { deliveryMan }>
+
+export class GetDeliveryManProfileUseCase {
+  constructor(private deliveryMenRepository: DeliveryMenRepository) {}
+
+  async execute({
+    deliveryManId,
+  } : GetDeliveryManProfileUseCaseRequest): Promise<GetDeliveryManProfileUseCaseResponse> {
+    const deliveryMan = await this.deliveryMenRepository.findById(deliveryManId)
+
+    if(!deliveryMan) {
+      return left(new ResourceNotFoundError)
+    }
+
+    return right({ deliveryMan })
+  }
+}
