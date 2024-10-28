cf37beb874126e413aa96083034033f633d11d92
Gustavo Garcia <gustavogarcia@sejaimpulse.com>
2024-10-28 20:13:24 -0300

    feat: authenticate deliverer docs

--- src/infra/http/controllers/deliverers/authenticate-deliverer.controller.ts
+++ src/infra/http/controllers/deliverers/authenticate-deliverer.controller.ts
@@ -4,14 +4,17 @@ import { ZodValidationPipe } from "../../pipes/zod-validation-pipe";
 import { z } from "zod";
 import { BadRequestError } from "@/core/errors/bad-request-error";
 import { Public } from "@/infra/auth/public";
+import { ApiBody, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { AuthenticateDelivererRequestDTO, AuthenticateDelivererResponseDTO } from "./docs/authenticate-deliverer.controller.docs";
 
 const authenticateDelivererSchema = z.object({
   cpf: z.string(),
   password: z.string().min(6),
 })
 
-type AuthenticateDelivererSchema = z.infer<typeof authenticateDelivererSchema>
+export type AuthenticateDelivererSchema = z.infer<typeof authenticateDelivererSchema>
 
+@ApiTags("Deliverers")
 @Controller()
 export class AuthenticateDelivererController {
   constructor(private authenticateDelivererUseCase: AuthenticateDelivererUseCase) {}
@@ -20,6 +23,8 @@ export class AuthenticateDelivererController {
   @HttpCode(201)
   @Public()
   @UsePipes(new ZodValidationPipe(authenticateDelivererSchema))
+  @ApiBody({ type: AuthenticateDelivererRequestDTO })
+  @ApiResponse({ status: 201, type: AuthenticateDelivererResponseDTO })
   async handle(
     @Body() body: AuthenticateDelivererSchema
   ) {
